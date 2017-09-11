c201c85ac83d24912ccf49dc69c74f0ef527a49d
Robert Yuen <dungeonry@hotmail.com>
2017-09-10 17:58:35 -0700

    it builds

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+        "index": path.join(__dirname, "src/index.jsx")
+    },
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module:{
+        loaders: [{
+            test: /\.jsx$/,
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.join(__dirname, "src/templates/index.html"),
+            filename: "index.html",
+            inject: "body",
+            title: "YVRUN"
+        })
+    ]
+};
