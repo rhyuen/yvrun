12b23e39a06d7c5f89abf04405fc27e43bae0eac
Robert Yuen <dungeonry@hotmail.com>
2017-09-11 14:53:39 -0700

    css changes and media queries.

--- src/app.jsx
+++ src/app.jsx
@@ -3,8 +3,13 @@ import React, {Component} from "react";
 class App extends Component{
     render(){
         return (
-            <div>
-                main app thing.
+            <div className = "root">
+                <div className = "root__left">
+                    Left Panel
+                </div>
+                <div className = "root__main">
+                    Right Panel
+                </div>
             </div>
         );
     }
