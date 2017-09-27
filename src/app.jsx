69cf9ae2b09eeddd279c636299dd1191705af8fc
Robert Yuen <dungeonry@hotmail.com>
2017-09-26 17:07:28 -0700

    Css hover changes and more consisten colouring.

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import Dayblock from "./dayblock.jsx";
 import Nav from "./nav.jsx";
+import MainSplash from "./mainsplash.jsx";
 import Footer from "./footer.jsx";
 import runData from "./data.json";
 
@@ -28,10 +29,7 @@ class App extends Component{
             <div className = "root">
                 <Nav days = {runData.days}/>               
                 <div className = "root__main">
-                    <div className = "root__main__title">YVRUN, Running with Strangers</div>
-                    <div className = "root__main__content">Here's a list of days, times and places where strangers run together.  It's all over town.  At all times of day.  Some are even at some ungodly hour of the morning.</div>
-                    <div className = "root__main__content">Send more groups of strangers at <a href = "/">yvrun@gmail.com</a></div>                    
-
+                    <MainSplash/>
                     {
                         runData.days.map((item) => {
                             return (
---
+++ src/mainsplash.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default (props) => (
+    <div>
+        <div className = "root__main__title">YVRUN, Running with Strangers</div>
+        <div className = "root__main__content">Here's a list of days, times and places where strangers run together.  It's all over town.  At all times of day.  Some are even at some ungodly hour of the morning.</div>
+        <div className = "root__main__content">
+            Send more groups of strangers <a href = "mailto:alloauth@gmail.com">here</a>.            
+        </div>                    
+    </div>
+);
